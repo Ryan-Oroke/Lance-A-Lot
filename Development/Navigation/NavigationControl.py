de653d6efd95cba63a8767fdef922ff750be57c6
ClassySmashley <50534076+ClassySmashley@users.noreply.github.com>
2021-03-29 20:28:51 -0600

    Update NavigationControl.py
    WIP Do not use

--- Development/Navigation/NavigationControl.py
+++ Development/Navigation/NavigationControl.py
@@ -1,26 +1,29 @@
 import smbus
+import sys
 import time
 import os
 from ultrasonics import UltrasonicSensors
 from i2c_commands import i2c_control
+sys.path.append("../modules/")
+import I2C_LIB
+
+std_spd = 100
 
 try:
     while True:
-        center = 0
-        for i in range(3)
-            r_dist = UltrasonicSensors.Left_dis()
-            l_dist = UltrasonicSensors.Right_dis()
-            center = center + r_dist - l_dist
-        center = center/3 
-        if center < 1 # if center is less than one then left distance was larger on average
-            # twist left and continue forward
-            i2c_control.Send_Command("AF100")
-            i2c_control.Send_Command("BR100")
-            time.sleep(0.5)
-            i2c_control.Send_Command("AF100")
-            i2c_control.Send_Command("BF100")
-        if center > 1 # if center is > 1 right distance was larger
-            # twist right and continue forward
+        #Beginning of navigation loop will always be to go forward
+        driverobot("F"
+        center = centering()
+        
+            if center < 1 # if center is less than one then left distance was larger on average
+                # twist left and continue forward
+                i2c_control.Send_Command("AF100")
+                i2c_control.Send_Command("BR100")
+                time.sleep(0.5)
+                i2c_control.Send_Command("AF100")
+                i2c_control.Send_Command("BF100")
+            if center > 1 # if center is > 1 right distance was larger
+                # twist right and continue forward
     # Reset by pressing CTRL + C
 except KeyboardInterrupt:
     print("Measurement stopped by User")
