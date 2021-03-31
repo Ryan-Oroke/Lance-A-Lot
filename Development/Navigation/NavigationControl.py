19ff2b63856e827ae362454f59d6f36338c79795
ClassySmashley <50534076+ClassySmashley@users.noreply.github.com>
2021-03-31 16:46:59 -0600

    Update NavigationControl.py
    fixed first set of syntax errors

--- Development/Navigation/NavigationControl.py
+++ Development/Navigation/NavigationControl.py
@@ -21,31 +21,31 @@ try:
         driveRobot(F, std_spd)
         center = centering()  
         left_on, center_on, right_on = IR_read()
-        if right_on == True
+        if right_on == True:
             turnRobot(L, std_spd, turn_delay)
             driveRobot(F, std_spd/2)
             rightLast = True
             leftLast = False
-        else if left_on == True
+        else if left_on == True:
             turnRobot(R, std_spd, turn_delay)
             driveRobot(F, std_spd/2)
             leftLast = True
             rightLast = False
-        else if center_on == True
+        else if center_on == True:
             stopRobot()
             driveRobot(B, std_spd)
             time.sleep(1)
-            if leftLast == True
+            if leftLast == True:
                 turnRobot(R, std_spd, turn_delay)
                 driveRobot(F, std_spd/2)
-            else if rightLast == True
+            else if rightLast == True:
                 turnRobot(L, std_spd, turn_delay)
                 driveRobot(F, std_spd/2)
-        else if center < 1 # if center is less than one then left distance was larger on average
+        else if center < 1: # if center is less than one then left distance was larger on average
             #turn left
             turnRobot(L, std_spd, turn_delay)
             driveRobot(F, std_spd)
-        else if center > 1 # if center is > 1 right distance was larger
+        else if center > 1: # if center is > 1 right distance was larger
             #turn right
             turnRobot(R, std_spd, turn_delay)
             driveRobot(F, std_spd)
